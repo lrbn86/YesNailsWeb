bd91a588c4ac17ed44ec6e35e8a36faa74eb61ef
Brandon Nguyen <lrbn86@gmail.com>
2019-07-26 18:41:52 -0400

    Icon test

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
   <title>Yes Nails</title>
   <link rel="stylesheet" href="index.css">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="apple-touch-icon" sizes="192x192" href="YNLogo.png">
 </head>
 <body>
 
