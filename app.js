c53c886aed1f887e0423b81cf8e7eff795aafba5
Brandon Nguyen <lrbn86@gmail.com>
2019-07-25 23:42:57 -0400

    Getting started on the iPad page.

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-alert("Test");
+const phoneBtn = document.getElementById("phoneBtn");
+const padBtn = document.getElementById("padBtn");
+
+phoneBtn.addEventListener('click', function() {
+  window.location.href = "iphone-page.html";
+});
+
+padBtn.addEventListener('click', function() {
+  window.location.href = "ipad-page.html";
+});
